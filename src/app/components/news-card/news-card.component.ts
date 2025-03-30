3b6dc9e69b08079fc8f651876e667743eda04871
Reginaldo Junior <reginaldojr.dev@gmail.com>
2025-03-30 01:25:48 -0300

    base do projeto

---
+++ src/app/components/news-card/news-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-card',
+  imports: [],
+  templateUrl: './news-card.component.html',
+  styleUrl: './news-card.component.css'
+})
+export class NewsCardComponent {
+
+}
---
+++ src/app/components/news-section/news-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-section',
+  imports: [],
+  templateUrl: './news-section.component.html',
+  styleUrl: './news-section.component.css'
+})
+export class NewsSectionComponent {
+
+}
